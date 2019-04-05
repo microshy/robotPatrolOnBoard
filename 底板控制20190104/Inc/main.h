c662308574b747cad21e0a9f15a0b5a2e20aca3b
SeanX <microshy@users.noreply.github.com>
2019-04-05 22:42:56 +0800

    Revert "Revert "ver。190107""
    This reverts commit 424df6c3beb28bf9d720f9975ccbd5fe8eb1d515.

---
+++ 底板控制20190104/Inc/main.h
@@ -0,0 +1,39 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+/* 包含头文件 ----------------------------------------------------------------*/
+/* 类型定义 --------------------------------------------------------------*/
+/* 宏定义 --------------------------------------------------------------------*/
+
+#define USE_DHCP       /* enable DHCP, if disabled static address is used */
+
+#define DEST_IP_ADDR0               192
+#define DEST_IP_ADDR1               168
+#define DEST_IP_ADDR2                 3
+#define DEST_IP_ADDR3               100
+
+/* USER CODE BEGIN Includes */
+#define IP_ADDR0   (uint8_t) 192
+#define IP_ADDR1   (uint8_t) 168
+#define IP_ADDR2   (uint8_t) 3
+#define IP_ADDR3   (uint8_t) 101
+   
+/*NETMASK*/
+#define NETMASK_ADDR0   (uint8_t) 255
+#define NETMASK_ADDR1   (uint8_t) 255
+#define NETMASK_ADDR2   (uint8_t) 255
+#define NETMASK_ADDR3   (uint8_t) 0
+
+/*Gateway Address*/
+#define GW_ADDR0   (uint8_t) 192
+#define GW_ADDR1   (uint8_t) 168
+#define GW_ADDR2   (uint8_t) 3
+#define GW_ADDR3   (uint8_t) 1
+
+/* 扩展变量 ------------------------------------------------------------------*/
+/* 函数声明 ------------------------------------------------------------------*/
+
+
+
+#endif /* __MAIN_H */
+
+/******************* (C) COPYRIGHT 2015-2020 硬石嵌入式开发团队 *****END OF FILE****/
